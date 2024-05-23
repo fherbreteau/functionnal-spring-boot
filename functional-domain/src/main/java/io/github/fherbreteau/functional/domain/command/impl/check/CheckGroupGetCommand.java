a1ff5d8b32a7601dbcecb0befe3f7369e0d1d676
François HERBRETEAU <72505172+fherbreteau@users.noreply.github.com>
2024-05-23 02:46:50 +0200

    feat(): Fix code duplication

--- functional-domain/src/main/java/io/github/fherbreteau/functional/domain/command/impl/check/CheckGroupGetCommand.java
+++ functional-domain/src/main/java/io/github/fherbreteau/functional/domain/command/impl/check/CheckGroupGetCommand.java
@@ -1,52 +1,23 @@
 package io.github.fherbreteau.functional.domain.command.impl.check;
 
-import io.github.fherbreteau.functional.domain.command.impl.error.UserErrorCommand;
 import io.github.fherbreteau.functional.domain.command.impl.success.GroupGetCommand;
-import io.github.fherbreteau.functional.domain.entities.User;
 import io.github.fherbreteau.functional.domain.entities.UserCommandType;
-import io.github.fherbreteau.functional.domain.entities.UserInput;
 import io.github.fherbreteau.functional.driven.GroupRepository;
 import io.github.fherbreteau.functional.driven.PasswordProtector;
 import io.github.fherbreteau.functional.driven.UserChecker;
 import io.github.fherbreteau.functional.driven.UserRepository;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
-import static java.util.Objects.nonNull;
-
-public class CheckGroupGetCommand extends AbstractCheckUserCommand<GroupGetCommand> {
-    private final String name;
-    private final UUID userId;
+public class CheckGroupGetCommand extends AbstractCheckGetInfoCommand<GroupGetCommand> {
 
     public CheckGroupGetCommand(UserRepository userRepository, GroupRepository groupRepository, UserChecker userChecker,
                                 PasswordProtector passwordProtector, String name, UUID userId) {
-        super(userRepository, groupRepository, userChecker, passwordProtector);
-        this.name = name;
-        this.userId = userId;
-    }
-
-    @Override
-    protected List<String> checkAccess(User actor) {
-        List<String> reasons = new ArrayList<>();
-        if (nonNull(name) && !userRepository.exists(name)) {
-            reasons.add(String.format("%s is missing", name));
-        }
-        if (nonNull(userId) && !userRepository.exists(userId)) {
-            reasons.add(String.format("%s is missing", userId));
-        }
-        return reasons;
+        super(userRepository, groupRepository, userChecker, passwordProtector, name, userId, UserCommandType.GROUPS);
     }
 
     @Override
     protected GroupGetCommand createSuccess() {
         return new GroupGetCommand(userRepository, groupRepository, passwordProtector, name, userId);
-    }
-
-    @Override
-    protected UserErrorCommand createError(List<String> reasons) {
-        UserInput input = UserInput.builder(name).withUserId(userId).build();
-        return new UserErrorCommand(UserCommandType.GROUPS, input, reasons);
     }
 }
